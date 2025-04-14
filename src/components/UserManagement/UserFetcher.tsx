5cf74b1d5884c7cc45e1d28dd6edcec833c5d6a1
Nhat Phan <pmnhat95@gmail.com>
2025-04-14 22:44:15 +0700

    Bai 3: search user

--- src/components/UserManagement/UserFetcher.tsx
+++ src/components/UserManagement/UserFetcher.tsx
@@ -9,6 +9,7 @@ export function UserFetcher() {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -21,17 +22,28 @@ export function UserFetcher() {
     .finally(() => setLoading(false));
   }, []);
 
+  const filteredUsers = users.filter(user => user.name.toLowerCase().includes(searchTerm.toLowerCase().trim()));
+
   if (loading) return <p>Loading data....</p>
   if (error) return <p style={{color: 'red'}}>Loi roi huhu {error}</p>
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Tìm theo tên..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ marginBottom: '16px', padding: '8px' }}
+      />
       <ul>
-        {users.map(user => (
-          <li key={user.id}>
+        {filteredUsers.map(user => (
+          <li key={user.id} style={{ marginBottom: '8px' }}>
             <strong>{user.name}</strong> - {user.email}
           </li>
         ))}
+
+        {!filteredUsers.length && <p>Khong co data nao phu hop voi tim kiem cua ban</p>}
       </ul>
     </div>
   );
